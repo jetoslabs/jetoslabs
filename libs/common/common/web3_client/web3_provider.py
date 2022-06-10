1ca6d14de7fc08616cbc33b281f0c401c431b21a
Anurag Jha <toanuragjha@gmail.com>
2022-06-09 22:30:11 -0700

    Common: chore

--- libs/common/common/web3_client/web3_provider.py
+++ libs/common/common/web3_client/web3_provider.py
@@ -3,7 +3,7 @@ from starlette import status
 from web3 import Web3
 
 
-def get_w3_provider(uri: str) -> Web3:
+def new_w3_provider(uri: str) -> Web3:
     if not uri or len(uri) == 0:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
